bcf285d50141c85778b09e35fbaea2c836e339a2
Justino <justinhjy1004@gmail.com>
2025-09-01 11:44:31 +0800

    implemented topological sort

--- datastructs/node.go
+++ datastructs/node.go
@@ -1,6 +1,10 @@
 package datastructs
 
-import "math"
+import (
+	"errors"
+	"math"
+	"slices"
+)
 
 type Node struct {
 	Value              float64
@@ -23,6 +27,7 @@ func Input(x float64) Node {
 
 func Mul(x Node, y Node) Node {
 
+	// Only fo constants though
 	value := x.Value * y.Value
 	operation := "mul"
 	grad_wrt_parents := []float64{y.Value, x.Value}
@@ -34,17 +39,128 @@ func Mul(x Node, y Node) Node {
 
 }
 
-// TODO
 func Add(x Node, y Node) Node {
-	return Node{}
+
+	value := x.Value + y.Value
+	operation := "add"
+	grad_wrt_parents := []float64{1, 1}
+	parents := []Node{x, y}
+
+	result := Node{Value: value, Operation: operation, Grad_wrt_parents: grad_wrt_parents, Parents: parents, Partial_derivative: 0}
+
+	return result
+}
+
+func Sub(x Node, y Node) Node {
+
+	value := x.Value - y.Value
+	operation := "sub"
+	grad_wrt_parents := []float64{1, -1}
+	parents := []Node{x, y}
+
+	result := Node{Value: value, Operation: operation, Grad_wrt_parents: grad_wrt_parents, Parents: parents, Partial_derivative: 0}
+
+	return result
 }
 
 // TODO
-func Log(x Node) Node {
-	return Node{}
+func Log(x Node) (Node, error) {
+
+	if x.Value <= 0 {
+		return Node{}, errors.New("Input of log is <= 0")
+	}
+
+	value := math.Log(x.Value)
+	operation := "log"
+	grad_wrt_parents := []float64{1 / x.Value}
+	parents := []Node{x}
+
+	result := Node{Value: value, Operation: operation, Grad_wrt_parents: grad_wrt_parents, Parents: parents}
+
+	return result, nil
+
 }
 
-// TODO
 func Sin(x Node) Node {
-	return Node{Value: math.Sin(x.Value)}
+
+	value := math.Sin(x.Value)
+	operation := "sin"
+	grad_wrt_parents := []float64{math.Cos(x.Value)}
+	parents := []Node{x}
+
+	result := Node{Value: value, Operation: operation, Grad_wrt_parents: grad_wrt_parents, Parents: parents}
+
+	return result
+}
+
+func Cos(x Node) Node {
+
+	value := math.Cos(x.Value)
+	operation := "cos"
+	grad_wrt_parents := []float64{-math.Sin(x.Value)}
+	parents := []Node{x}
+
+	result := Node{Value: value, Operation: operation, Grad_wrt_parents: grad_wrt_parents, Parents: parents}
+
+	return result
+}
+
+func inductive_topological_sort(n Node, result *[]*Node) {
+
+	if len(n.Parents) == 0 {
+
+		if !slices.Contains(*result, &n) {
+			*result = append(*result, &n)
+		}
+
+		return
+
+	}
+
+	for _, parent := range n.Parents {
+
+		if !slices.Contains(*result, &parent) {
+			inductive_topological_sort(parent, result)
+		}
+	}
+
+	*result = append([]*Node{&n}, *result...)
+
+}
+
+// Given a single output, backward trace the dependencies
+func Base_topological_sort(n Node) []*Node {
+
+	result := []*Node{}
+
+	for _, parent := range n.Parents {
+
+		if !slices.Contains(result, &parent) {
+			inductive_topological_sort(parent, &result)
+		}
+
+	}
+
+	result = append([]*Node{&n}, result...)
+
+	return result
+}
+
+// TODO
+func Dependencies(nodes []Node) [][]Node {
+
+	// initialize slice matrix
+	num_input := len(nodes)
+	descendents := make([][]Node, len(nodes))
+
+	for i := 0; i < num_input; i++ {
+		descendents[i] = append(descendents[i], nodes[i])
+	}
+
+	// for each n
+
+	//var n Node = nodes[0]
+
+	return [][]Node{}
+
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/justinhjy1004/go-autodiff/datastructs"
 )
@@ -14,7 +15,17 @@ func main() {
 	n2 := datastructs.Input(4)
 	n3 := datastructs.Input(0.5)
 
-	v1 := datastructs.Mul(n3, datastructs.Mul(n1, n2))
+	LogN2, err := datastructs.Log(n2)
 
-	fmt.Println(v1)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	v1 := datastructs.Add(LogN2, datastructs.Mul(datastructs.Sin(n3), datastructs.Mul(n1, n2)))
+
+	fmt.Println(datastructs.Base_topological_sort(v1))
+
+	for _, n := range datastructs.Base_topological_sort(v1) {
+		fmt.Printf("%s, %f \n", n.Operation, n.Value)
+	}
 }
